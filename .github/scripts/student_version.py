542b2c5da3d0b779738ec5a1dfb2b34a25e18125
benco <b.cohenboulakia@gmail.com>
2025-05-05 18:18:01 +0200

    GPL-3 licence added

--- .github/scripts/student_version.py
+++ .github/scripts/student_version.py
@@ -1,3 +1,19 @@
+"""
+This file is part of nbworkshop.
+
+nbworkshop is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3 as published by
+the Free Software Foundation.
+
+nbworkshop is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with nbworkshop. If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import argparse
 import json
 import os
